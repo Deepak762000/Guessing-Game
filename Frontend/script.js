f4d04e32d11f31918fe999721ea11faac2980bc6
Deepak <dnegi9999@gmail.com>
2025-09-07 13:46:02 +0530

    Fixed

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,11 +1,11 @@
 async function makeGuess() {
   const guess = document.getElementById("guess").value;
-  if (!guess) return; // agar input empty ho
+  if (!guess) return;
 
   try {
     const res = await fetch(`http://localhost:8080/check/${guess}`);
     if (!res.ok) throw new Error("Server error");
-    const data = await res.json(); // JSON parse
+    const data = await res.json(); 
 
     const msgEl = document.getElementById("message");
     if (data.result === "low") msgEl.textContent = "Too Low! 🔽";
